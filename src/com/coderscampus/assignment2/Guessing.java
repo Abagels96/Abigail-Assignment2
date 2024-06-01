81bd793977642d1e83e3be4ed5c999a008dd7c12
DrStein1221 <133988281+DrStein1221@users.noreply.github.com>
2024-06-01 11:09:31 -0500

    still working on it

--- src/com/coderscampus/assignment2/Guessing.java
+++ src/com/coderscampus/assignment2/Guessing.java
@@ -4,43 +4,49 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class  Guessing {
-Integer scan=chooseNumber();
-Integer guessingNumber=chooseNumber();
 
-Integer chooseNumber() {
-	Random random= new Random();
-	int guessingNumber=random.nextInt(1,101);
+	Integer scan;
+boolean a;
+Random random= new Random();
 	
+int guessingNumber=random.nextInt(1,101);
+
+
+	
+	
+Integer chooseNumber( ) {
+	System.out.println(guessingNumber);
 	
 	Scanner scanner= new Scanner(System.in);
 	System.out.println("Type in your guess");
 	String scanning= scanner.nextLine();	
 	
-	
+	//where input is received hopefully runs five times 
 	Integer scan=Integer.parseInt(scanning); //changing the input to an integer
 	
-	if( scan==guessingNumber) {
+
+
+		if( scan==guessingNumber) {
 		System.out.println("You win");
+	 
 		
-	}
+		}
 	else if(scan>=1&& scan<guessingNumber) {
 		System.out.println("Please pick a higher number");
+		
 	}
 	else if(scan<100 && scan>guessingNumber) {
 		System.out.println("Please pick a lower number");
+		
 	}
-	else { System.out.println("Your guess is not between 1 and 100 please pick again");
-	}
+	else { System.out.println("Your guess is not between 1 and 100 please pick again");}
 	
-	return guessingNumber;
-}
+return scan;}
 
 
-	
-	Integer getNumber() {
-	return scan;}
-	
+
+		
 	String displayNumber(String a, Integer guessingNumber) {
-		return "The number you are trying to guess is: " +guessingNumber;
+		return  a +guessingNumber;
 	}
 }
--- src/com/coderscampus/assignment2/Main.java
+++ src/com/coderscampus/assignment2/Main.java
@@ -5,18 +5,26 @@ public class Main {
         Guessing guessing = new Guessing();
         
         for (int i = 0; i < 5; i++) {
+        	guessing.chooseNumber();
+        	
+ Integer input=guessing.scan;
+ Integer number=guessing.guessingNumber;
+       boolean firstOne= input==number;
+    
+       if(firstOne=true) {
+    	   break;
+       }
+          }
+       
+        	
+     
+       }
+      
+          
            
-            guessing.chooseNumber();
-            
-            
-            Integer variable = guessing.getNumber();
-            
-            if (variable > 100 || variable < 1) {
-                i--;  // Decrement the loop counter to retry
-            } else {}
             	
             	
-            guessing.displayNumber("This is the number you were looking for", variable);    
+               
         }
-    }
-}
+    
+
